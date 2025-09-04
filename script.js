f67682a49563d36fe256ffc0bf136bb82529b028
Gabriel <gabe.bincheski@gmail.com>
2025-09-04 16:34:44 -0300

    adicionado animação de carregamanto de pagina

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-// script do menu dropdown
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("menu-button");
   const menu = document.getElementById("menu-options");
-  const links = menu.querySelectorAll("a"); // Seleciona todos os links do menu
+  const links = menu.querySelectorAll("a");
 
-  // Alterna o menu ao clicar no botão
   button.addEventListener("click", function () {
     menu.classList.toggle("active");
+    button.classList.toggle("open");
   });
 
-  // Fecha o menu ao clicar fora dele
   document.addEventListener("click", function (event) {
     if (!button.contains(event.target) && !menu.contains(event.target)) {
       menu.classList.remove("active");
+      button.classList.remove("open");
     }
   });
 
-  // Fecha o menu ao clicar em qualquer link dentro dele
   links.forEach(function (link) {
     link.addEventListener("click", function () {
       menu.classList.remove("active");
+      button.classList.remove("open");
     });
   });
+});
+
+window.addEventListener("load", function () {
+  const loader = document.getElementById("loader");
+  loader.style.display = "none";
 });
